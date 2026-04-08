02fea854aad69645a0f6d97c6e86b9f4c7530e43
ZomaX6 <44568606+ZomaX6@users.noreply.github.com>
2026-04-08 14:18:46 +0200

    Add ACMAupdateLocal.ps1 script for installation of ACMA

---
+++ ACMAupdateLocal.ps1
@@ -0,0 +1,66 @@
+param (
+    [string]$ACMAuri = "\\rsbe01ms0014\data$\Instalacije\Microsoft\AzureConnectedMachineAgent.msi",
+    [string]$ACMAfile = "AzureConnectedMachineAgent.msi",
+    [string]$ACMAlog = "C:\Program Files\AzureConnectedMachineAgent\agent_install.log"
+)
+
+$ErrorActionPreference = "Stop"
+function Write-Log {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory = $true, Position=0)]
+        [ValidateSet("ERROR", "INFO", "SUCCESS")]
+        [ValidateNotNull()]
+        [string]$Level,
+        [parameter(Mandatory = $true, Position=1)]
+        [string]$String,
+        [parameter(Position=2)]
+        [string]$FileName = "ACMA_update", #Change log filename, should uniquely refer to the script
+        [parameter(Position=3)]
+        [bool]$ConsoleVerbose = $true #Verbose output in console
+    )
+    $Date = (Get-Date).ToString("dd.MM.yyyy HH:mm:ss.fff")
+    $LogDate = (Get-Date).ToString("yyyy-MM-dd")
+    $LogFileName = "{0}_{1}.txt" -f $LogDate, $FileName
+    $LogFile = Join-Path $PSScriptRoot -ChildPath "ScriptLogs\$LogFileName"
+    if (-not (Test-Path "$PSScriptRoot\ScriptLogs")) {
+        New-Item -Path $PSScriptRoot -Name ScriptLogs -ItemType Directory
+    }
+    $Output = "{0}|{1}|{2}|{3}" -f $Date, $FileName, $Level, $String
+    $Output | Out-File $LogFile -Append
+    if ($ConsoleVerbose) {
+        Write-Verbose $Output -Verbose
+    }
+}
+
+Write-Log INFO "Skripta startovana"
+
+try {
+    Copy-Item -Path $ACMAuri -Destination $ACMApath
+    Write-Log SUCCESS "Instalacija skinuta"
+}
+catch {
+    Write-Log ERROR "Dogodila se greska prilikom skidanja instalacije | Error: $_"
+    Write-Log INFO "Skripta zavrsena"
+    exit
+}
+
+$ACMApath = Join-Path $env:TEMP -ChildPath $ACMAfile
+$ACMAargs = "/i `"$ACMApath`" /qn /l*v `"$ACMAlog`""
+try {
+    Start-Process msiexec.exe -ArgumentList $ACMAargs -NoNewWindow -Wait
+    Write-Log SUCCESS "Instalacija uspesno trigerovana, za detaljan log pogledaj $ACMAlog"
+}
+catch {
+    Write-Log ERROR "Dogodila se neka greska prilikom trigerovanja instalacije | Error: $_"
+}
+
+try {
+    Remove-Item -Path $ACMApath -Force
+    Write-Log SUCCESS "Instalacija obrisana"
+}
+catch {
+    Write-Log ERROR "Dogodila se greska prilikom brisanja instalacije | Error: $_"
+}
+
+Write-Log INFO "Skripta zavrsena"
